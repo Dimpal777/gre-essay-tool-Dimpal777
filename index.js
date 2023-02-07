fc751088867cd21c32a3443129a4f6f3cd8bdfa0
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-02-07 10:31:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,8 @@
 // We will understand all of this later in the course.
 // DO NOT MODIFY THIS FILE
 
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+function updateWordCount() {
+    var text = document.getElementById("evaluatedText").value;
+        var wordCount = text.trim() ? text.trim().split(" ").length : 0;
+        document.getElementById("wordCount").innerHTML = wordCount;
+}
