8d2057d30ef7c3bc3f177951157f5e6dce70a614
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-02-07 10:24:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+unction updateWordCount() {
+    var text = document.getElementById("evaluatedText").value;
+        var wordCount = text.trim() ? text.trim().split(" ").length : 0;
+        document.getElementById("wordCount").innerHTML = wordCount;
+}
